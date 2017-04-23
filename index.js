6bb6343eefa04a845aeccd671f7b9430b39d5c99
dneustadt <kontakt@davidneustadt.de>
2017-04-24 00:24:49 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,53 @@
+const fs = require('fs');
+const color_indexes = {
+    0: 'black',
+    1: 'red',
+    2: 'green',
+    3: 'yellow',
+    4: 'blue',
+    5: 'magenta',
+    6: 'cyan',
+    7: 'white',
+    8: 'lightBlack',
+    9: 'lightRed',
+    10: 'lightGreen',
+    11: 'lightYellow',
+    12: 'lightBlue',
+    13: 'lightMagenta',
+    14: 'lightCyan',
+    15: 'lightWhite'
+};
+const setColor = function(data, count, colors) {
+    colors[color_indexes[count]] = data;
+};
+const getWalColorConfig = function(input, setColor, config) {
+    var lines = input.toString().split('\n'),
+        colors = config.colors;
+    for (var i = 0; i < lines.length; i++) {
+        setColor(lines[i], i, colors);
+    }
+
+    var backgroundColor = colors.black,
+        foregroundColor = colors.lightWhite,
+        cursorColor = foregroundColor,
+        borderColor = backgroundColor;
+
+    return Object.assign({}, config, {
+        foregroundColor, // foreground color
+        backgroundColor, // background color
+        borderColor, // border color
+        cursorColor, // cursor color
+        colors
+    })
+};
+const file_path = require('os').homedir() + '/.cache/wal/colors';
+const input = fs.existsSync(file_path) ? fs.readFileSync(file_path) : null;
+
+exports.decorateConfig = config => {
+    if (input) {
+        return getWalColorConfig(input, setColor, config);
+    } else {
+        console.log('Couldn\'t find wal cached colors. Run wal first.');
+        return Object.assign({}, config, {});
+    }
+};
